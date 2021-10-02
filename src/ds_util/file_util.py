6f0b3a2b994909d3fc3e9da89e7191032e57c509
kiccho1101 <youodf11khp@gmail.com>
2021-10-02 16:14:09 +0900

    0.6.0: add logger to TimeUtil.timer

--- src/ds_util/file_util.py
+++ src/ds_util/file_util.py
@@ -10,45 +10,55 @@ from ds_util.time_util import TimeUtil
 
 class FileUtil:
     @staticmethod
-    def load_csv(filepath: Union[str, Path], verbose: bool = True, **kwargs):
+    def load_csv(
+        filepath: Union[str, Path], verbose: bool = True, logger=None, **kwargs
+    ):
         if verbose:
-            with TimeUtil.timer(f"Read {str(filepath)}"):
+            with TimeUtil.timer(f"Read {str(filepath)}", logger):
                 return pd.read_csv(filepath, **kwargs)
         return pd.read_csv(filepath, **kwargs)
 
     @staticmethod
-    def save_npy(arr: np.ndarray, filepath: Union[str, Path], verbose: bool = True):
+    def save_npy(
+        arr: np.ndarray, filepath: Union[str, Path], verbose: bool = True, logger=None
+    ):
         with open(filepath, "wb") as f:
             if verbose:
-                with TimeUtil.timer(f"Save {str(filepath)}"):
+                with TimeUtil.timer(f"Save {str(filepath)}", logger):
                     np.save(f, arr)
             else:
                 np.save(f, arr)
 
     @staticmethod
-    def load_npy(filepath: Union[str, Path], verbose: bool = True) -> np.ndarray:
+    def load_npy(
+        filepath: Union[str, Path], verbose: bool = True, logger=None
+    ) -> np.ndarray:
         with open(filepath, "rb") as f:
             if verbose:
-                with TimeUtil.timer(f"Load {str(filepath)}"):
+                with TimeUtil.timer(f"Load {str(filepath)}", logger):
                     arr = np.load(f)
             else:
                 arr = np.load(f)
         return arr
 
     @staticmethod
-    def save_pickle(obj: Any, filepath: Union[str, Path], verbose: bool = True):
+    def save_pickle(
+        obj: Any, filepath: Union[str, Path], verbose: bool = True, logger=None
+    ):
         with open(filepath, "wb") as f:
             if verbose:
-                with TimeUtil.timer(f"Save {str(filepath)}"):
+                with TimeUtil.timer(f"Save {str(filepath)}", logger):
                     pickle.dump(obj, f)
             else:
                 pickle.dump(obj, f)
 
     @staticmethod
-    def load_pickle(filepath: Union[str, Path], verbose: bool = True) -> Any:
+    def load_pickle(
+        filepath: Union[str, Path], verbose: bool = True, logger=None
+    ) -> Any:
         with open(filepath, "rb") as f:
             if verbose:
-                with TimeUtil.timer(f"Load {str(filepath)}"):
+                with TimeUtil.timer(f"Load {str(filepath)}", logger):
                     obj = pickle.load(f)
             else:
                 obj = pickle.load(f)
--- src/ds_util/time_util.py
+++ src/ds_util/time_util.py
@@ -9,17 +9,25 @@ from ds_util.global_util import GlobalUtil
 class TimeUtil:
     @staticmethod
     @contextlib.contextmanager
-    def timer(name: str):
+    def timer(name: str, logger=None):
         t0, m0, p0 = GlobalUtil.get_metric()
-        print(f"[{name}] start [{m0:.1f}GB({p0:.1f}%)]")
+        if logger is not None:
+            logger.info(f"[{name}] start [{m0:.1f}GB({p0:.1f}%)]")
+        else:
+            print(f"[{name}] start [{m0:.1f}GB({p0:.1f}%)]")
         yield
         t1, m1, p1 = GlobalUtil.get_metric()
         delta = m1 - m0
         sign = "+" if delta >= 0 else "-"
         delta = math.fabs(delta)
-        print(
-            f"[{name}] done [{m1:.1f}GB({p1:.1f}%)({sign}{delta:.3f}GB)] {t1 - t0:.4f} s"
-        )
+        if logger is not None:
+            logger.info(
+                f"[{name}] done [{m1:.1f}GB({p1:.1f}%)({sign}{delta:.3f}GB)] {t1 - t0:.4f} s"
+            )
+        else:
+            print(
+                f"[{name}] done [{m1:.1f}GB({p1:.1f}%)({sign}{delta:.3f}GB)] {t1 - t0:.4f} s"
+            )
 
     @staticmethod
     def timer_wrapper(func: Callable):
