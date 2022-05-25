f8cf7c98511a914a19fdb2ecb3f5fc107039d045
yodai.a.kishimoto <yodai.a.kishimoto@rakuten.com>
2022-05-25 13:10:20 +0900

    fix logger util

--- src/ds_util/logger_util.py
+++ src/ds_util/logger_util.py
@@ -1,16 +1,13 @@
 import sys
-from pathlib import Path
 
 
 class LoggerUtil:
     @staticmethod
-    def get_logger(log_dir: Path, filename: str):
+    def get_logger(name: str):
         from loguru import logger
 
-        log_dir.mkdir(parents=True, exist_ok=True)
-        log_file = log_dir / filename
         logger.remove()
-        custom_format = "<green>[{time:YYYY-MM-DD HH:mm:ss}]</green> <level>{level} {message}</level>"
-        logger.add(log_file, format=custom_format)
+        custom_format = "<green>[{extra[name]} {time:YYYY-MM-DD HH:mm:ss}]</green> <level>{level} {message}</level>"
         logger.add(sys.stdout, colorize=True, format=custom_format)
+        logger = logger.bind(name=name)
         return logger
